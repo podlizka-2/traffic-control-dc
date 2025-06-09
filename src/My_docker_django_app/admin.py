bff1c3c0cd4ef606b5eb1feb98977c91f4ff9950
podlizka-2 <EYuNesterenko@yandex.ru>
2025-06-09 20:54:36 +0500

    рhh

--- src/My_docker_django_app/admin.py
+++
@@ -1,9 +0,0 @@
-# records/admin.py
-from django.contrib import admin
-from .models import Status, Type, Category, Subcategories, DDSRecords
-
-admin.site.register(Status)
-admin.site.register(Type)
-admin.site.register(Category)
-admin.site.register(Subcategories)
-admin.site.register(DDSRecords)
--- src/My_docker_django_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class My_docker_django_appConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_docker_django_app' 
